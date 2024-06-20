7798026c659dbcd1b9e86b103d900b6cb77280ae
Archanapradeep123 <147145430+Archanapradeep123@users.noreply.github.com>
2024-06-20 11:24:49 +0530

    Add files via upload

---
+++ day1/javascript/basics.js
@@ -0,0 +1,85 @@
+// console.log("hello world");
+
+// let x=10;
+// console.log(x);
+// {
+// let x=20;
+// console.log(x);
+// }
+// var a=4
+// console.log(typeof a);
+
+
+
+
+// var temp;
+// if (temp>30)
+// {
+
+//     console.log("hot day");
+// }
+// else if (temp==30)
+// {
+//     console.log("good day");
+
+// }
+// else
+// {
+//     console.log("cold day");
+// }
+
+
+// for(let i=0;i<10;i++)
+// {
+//     console.log(i);
+
+// }
+
+
+// let fruits=['apple','banana','orange'];
+// console.log(fruits);
+// fruits.push('grape')
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.unshift("pp")
+// console.log(fruits);
+
+
+
+// let person={
+//     firstname:'john',
+//     lastname:'doe',
+//     age:30,
+//     city:'idukki'
+// }
+// console.log(person);
+
+let people = [
+    {
+      name: 'Alice',
+      age: 25,
+      place: {
+        city: 'Wonderland',
+        country: 'Fairyland'
+      }
+    },
+    {
+      name: 'Bob',
+      age: 30,
+      place: {
+        city: 'TechCity',
+        country: 'Geekland'
+      }
+    },
+    {
+      name: 'Charlie',
+      age: 28,
+      place: {
+        city: 'Artsville',
+        country: 'Createland'
+      }
+    }
+  ];
+  console.log(people[2].place.country);
+
